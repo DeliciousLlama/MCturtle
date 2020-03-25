7a20a097e60fe7898c6e79dcf008bdbc7dd5f1ed
= <=>
2020-03-25 15:08:49 -0700

    added penUp and penDown function

--- execute.py
+++ execute.py
@@ -4,7 +4,6 @@ from mcpi.minecraft import Minecraft
 from math import *
 import time
 import enum
-from inspect import getblock
 
 # mc = Minecraft.create("52.8.34.121", 4711)
 mc = Minecraft.create("localhost")
@@ -30,19 +29,27 @@ class direction(enum.Enum):
     UP = 2
     DOWN = 3
     KEEP_SAME = 4
+    
+class heading(enum.Enum):
+    DOWN = 0
+    UP = 1
+    NORTH = 2
+    SOUTH = 3
+    WEST = 4
+    EAST = 5
 
 def headingToString(heading):
-    if heading == 0:
+    if heading == 0 or heading == heading.UP:
         return "Facing Down"
-    if heading == 1:
+    if heading == 1 or heading == heading.DOWN:
         return "Facing Up"
-    if heading == 2:
+    if heading == 2 or heading == heading.NORTH:
         return "Facing North"
-    if heading == 3:
+    if heading == 3 or heading == heading.SOUTH:
         return "Facing South"
-    if heading == 4:
+    if heading == 4 or heading == heading.WEST:
         return "Facing West"
-    if heading == 5:
+    if heading == 5 or heading == heading.EAST:
         return "Facing East"
 #----enums----
 
@@ -64,13 +71,14 @@ homeX = 0
 homeY = 0
 HomeZ = 0
 speed = 0.25
+incomeStorage = None
+trailStorage = None
 #------------PEN PROPERTIES------------
 
 
 #---------------PEN CONFIG---------------------
-def create_pen(cx, cy, cz):
-    # make a pen facing east    
-    mc.setBlock(cx, cy, cz, 33, 5)
+def createPen(cx, cy, cz):
+    # make a pen facing eas
     global rotation
     global x
     global y
@@ -78,16 +86,22 @@ def create_pen(cx, cy, cz):
     global homeX
     global homeY
     global homeZ
-    rotation = 5
+    global incomeStorage
+    global trailStorage
     x = int(cx)
     y = int(cy)
     z = int(cz)
+    rotation = 5
+    trailStorage = mc.getBlockWithData(cx, cy, cz)
+    incomeStorage = __get_block_on_side(rotation)
     homeX = x
     homeY = y
     homeZ = z
+    mc.setBlock(cx, cy, cz, 33, 5)
+    
 
 
-def update_pos(ux, uy, uz):
+def updatePos(ux, uy, uz):
     global x
     global y
     global z
@@ -96,7 +110,7 @@ def update_pos(ux, uy, uz):
     z = uz
 
 
-def update_stroke(block):
+def updateStroke(block):
     global strokeBlock
     strokeBlock = block
 
@@ -163,24 +177,64 @@ def __update_rotation(yawDirection, pitchDirection):
             
     #---------------PEN CONFIG---------------------
 
-def __get_block_infront():
-    global rotation
+def __get_block_on_side(direction, onBack = False):
     global x
     global y
     global z
-    if rotation == 5:
-        return mc.getBlock(x+1,y,z)
-    if rotation == 4:
-        return mc.getBlock(x-1,y,z)
-    if rotation == 3:
-        return mc.getBlock(x,y,z+1)
-    if rotation == 2:
-        return mc.getBlock(x,y,z+1)
-    if rotation == 1:
-        return mc.getBlock(x,y+1,z)
-    if rotation == 0:
-        return mc.getBlock(x,y-1,z)
+    
+    #onBack's function serves to reverse the direction of where it is getting the block from.
+    #Ex: direction = UP, onBack = True --> blockData(DOWN)
+    
+    if direction == 5 or direction == heading.EAST:
+        if not onBack:
+            return mc.getBlockWithData(x+1,y,z)
+        else:            
+            return mc.getBlockWithData(x-1,y,z)
+    if direction == 4 or direction == heading.WEST:
+        if not onBack:
+            return mc.getBlockWithData(x-1,y,z)
+        else:            
+            return mc.getBlockWithData(x+1,y,z)
+    if direction == 3 or direction == heading.SOUTH:
+        if not onBack:
+            return mc.getBlockWithData(x,y,z+1)
+        else:            
+            return mc.getBlockWithData(x,y,z-1)
+    if direction == 2 or direction == heading.NORTH:
+        if not onBack:
+            return mc.getBlockWithData(x,y,z-1)
+        else:            
+            return mc.getBlockWithData(x,y,z+1)
+    if direction == 1 or direction == heading.UP:
+        if not onBack:
+            return mc.getBlockWithData(x,y+1,z)
+        else:            
+            return mc.getBlockWithData(x,y-1,z)
+    if direction == 0 or direction == heading.DOWN:
+        if not onBack:
+            return mc.getBlockWithData(x,y-1,z)
+        else:            
+            return mc.getBlockWithData(x,y+1,z)
 
+#use param for positive and negative and xyz
+# def __get_block_behind():
+#     global rotation
+#     global x
+#     global y
+#     global z
+#     if rotation == 5:
+#         return mc.getBlockWithData(x-1,y,z)
+#     if rotation == 4:
+#         return mc.getBlockWithData(x+1,y,z)
+#     if rotation == 3:
+#         return mc.getBlockWithData(x,y,z-1)
+#     if rotation == 2:
+#         return mc.getBlockWithData(x,y,z+1)
+#     if rotation == 1:
+#         return mc.getBlockWithData(x,y-1,z)
+#     if rotation == 0:
+#         return mc.getBlockWithData(x,y+1,z)
+    
 def home(offsetX = 0, offsetY = 0, offsetZ = 0):
     global rotation
     global x
@@ -191,91 +245,116 @@ def home(offsetX = 0, offsetY = 0, offsetZ = 0):
     global homeZ
     mc.setBlock(homeX + offsetX, homeY + offsetY, homeZ + offsetZ, 33, rotation)
     mc.setBlock(x, y, z, strokeBlock)
-    update_pos(homeX + offsetX, homeY + offsetY, homeZ + offsetZ)
+    updatePos(homeX + offsetX, homeY + offsetY, homeZ + offsetZ)
     
 def forward(amount):
     global speed
+    global incomeStorage
+    global trailStorage
+    
+    incomeStorage = __get_block_on_side(rotation)    
     for i in range(0, amount):
     # move the piston in the specified rotation
+        if not isDown:
+            #store the block data infront into "incomeStorage"
+            #move forward and place block in "trailStorage"
+            #transfer the block data in "incomeStorage" into "trailStorage"
+            updateStroke(trailStorage)
+            
+    
         if rotation == 0:
             # down
             mc.setBlock(x, y - 1, z, 33, 0)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y - 1, z)
+            updatePos(x, y - 1, z)
             time.sleep(speed)
         if rotation == 1:
             # up
             mc.setBlock(x, y + 1, z, 33, 1)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y + 1, z)
+            updatePos(x, y + 1, z)
             time.sleep(speed)
         if rotation == 2:
             # north
             mc.setBlock(x, y, z - 1, 33, 2)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z - 1)
+            updatePos(x, y, z - 1)
             time.sleep(speed)
         if rotation == 3:
             # south
             mc.setBlock(x, y, z + 1, 33, 3)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z + 1)
+            updatePos(x, y, z + 1)
             time.sleep(speed)
         if rotation == 4:
             # west
             mc.setBlock(x - 1, y, z, 33, 4)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x - 1, y, z)
+            updatePos(x - 1, y, z)
             time.sleep(speed)
         if rotation == 5:
             # east
             mc.setBlock(x + 1, y, z, 33, 5)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x + 1, y, z)
+            updatePos(x + 1, y, z)
             time.sleep(speed)
+        
+        trailStorage = incomeStorage
+        incomeStorage = __get_block_on_side(rotation)
 
 def fd(amount):
     forward(amount)
 
 def backward(amount):
+    global incomeStorage
+    global trailStorage
+    
+    incomeStorage = __get_block_on_side(rotation, True)
     for i in range(0, amount):
-    # move the piston in the specified rotation
+        if not isDown:
+            #store the block data infront into "incomeStorage"
+            #move forward and place block in "trailStorage"
+            #transfer the block data in "incomeStorage" into "trailStorage"
+            updateStroke(trailStorage)
+        # move the piston in the specified rotation
         if rotation == 0:
             # down
             mc.setBlock(x, y + 1, z, 33, 0)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y + 1, z)
+            updatePos(x, y + 1, z)
             time.sleep(speed)
         if rotation == 1:
             # up
             mc.setBlock(x, y - 1, z, 33, 1)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y - 1, z)
+            updatePos(x, y - 1, z)
             time.sleep(speed)
         if rotation == 2:
             # north
             mc.setBlock(x, y, z + 1, 33, 2)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z + 1)
+            updatePos(x, y, z + 1)
             time.sleep(speed)
         if rotation == 3:
             # south
             mc.setBlock(x, y, z - 1, 33, 3)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z - 1)
+            updatePos(x, y, z - 1)
             time.sleep(speed)
         if rotation == 4:
             # west
             mc.setBlock(x + 1, y, z, 33, 4)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x + 1, y, z)
+            updatePos(x + 1, y, z)
             time.sleep(speed)
         if rotation == 5:
             # east
             mc.setBlock(x - 1, y, z, 33, 5)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x - 1, y, z)
+            updatePos(x - 1, y, z)
             time.sleep(speed)
+        trailStorage = incomeStorage
+        incomeStorage = __get_block_on_side(rotation, True)
 
 def bk(amount):
     backward(amount)
@@ -283,49 +362,85 @@ def bk(amount):
 def setx(newX):
     global x
     global rotation
+    global trailStorage
+    global incomeStorage
+    
     if newX > int(x):
+        incomeStorage = __get_block_on_side(heading.EAST)           
         for i in range(int(abs(newX-x))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x+1, y, z, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x+1, y, z)
+            updatePos(x+1, y, z)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.EAST)            
             time.sleep(speed)
     else:
+        incomeStorage = __get_block_on_side(heading.EAST, True)        
         for i in range(int(abs(newX-x))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x-1, y, z, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x-1, y, z)
+            updatePos(x-1, y, z)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.EAST, True)            
             time.sleep(speed)
             
 def sety(newY):
     global y
     global rotation
+    global trailStorage
+    global incomeStorage
+
     if newY > int(y):
+        incomeStorage = __get_block_on_side(heading.UP)            
         for i in range(int(abs(newY-y))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x, y+1, z, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y+1, z)
+            updatePos(x, y+1, z)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.UP)            
             time.sleep(speed)
     else:
+        incomeStorage = __get_block_on_side(heading.UP, True)            
         for i in range(int(abs(newY-y))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x, y-1, z, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y-1, z)
+            updatePos(x, y-1, z)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.UP, True)            
             time.sleep(speed)
             
 def setz(newZ):
     global z
     global rotation
     if newZ > int(z):
+        incomeStorage = __get_block_on_side(heading.SOUTH)            
         for i in range(int(abs(newZ-z))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x, y, z+1, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z+1)
+            updatePos(x, y, z+1)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.SOUTH)            
             time.sleep(speed)
     else:
+        incomeStorage = __get_block_on_side(heading.SOUTH, True)            
         for i in range(int(abs(newZ-z))):
+            if not isDown:
+                updateStroke(trailStorage)
             mc.setBlock(x, y, z-1, 33, rotation)
             mc.setBlock(x, y, z, strokeBlock)
-            update_pos(x, y, z-1)
+            updatePos(x, y, z-1)
+            trailStorage = incomeStorage
+            incomeStorage = __get_block_on_side(heading.SOUTH, True)            
             time.sleep(speed)
 
 def goto(newX, newY, newZ):
@@ -392,7 +507,7 @@ def penUp():
 def penDown():
     global isDown
     isDown = True
-def heading():
+def facing():
     global rotation
     return headingToString(rotation)
 def position():
@@ -400,13 +515,32 @@ def position():
     global y
     global z
     return (x,y,z)
+def setHeading(direction):
+    global rotation
+    if isinstance(direction, heading):
+        rotation = direction.value()
+        return
+    if type(direction) != int:
+        print('Error: direcction can only be integer or a heading enum. You passed in a ', type(direction),'. This step will not run due to the error.')
+        return TypeError
+    rotation = direction
 
 
-create_pen(px, py, pz)
-update_stroke(35)
+createPen(px, py-1, pz)
 time.sleep(3)
+updateStroke(1)
 
-goto(9786,60,9988)
+penUp()
+for i in range(4):
+    forward(14-i*2)
+    sety(61-i)
+    backward(13-i*2)
+    sety(55+i)
+backward(3)
+forward(3)
+penUp()
+goto(int(px), int(py-1), int(pz))
+# 9786 54 9991
 # for i in range(1,abs(9786 - 9779)+1):
 #     setx(9779+i);
 #     print('i', i)
